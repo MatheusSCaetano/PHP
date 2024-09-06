be9b34b4dcb5720ca36ed9e51fd5dee89a4fbd9c
Matheus Caetano <caetanomatheus142@gmail.com>
2024-09-05 23:02:00 -0400

    first

---
+++ modulo1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$var1 = "Matheus";
+$var2 = "Mathus";
+
+if($var1 == $var2){
+    echo '<div style="width:300px; height:200px; background:red;">  </div>';
+}else{
+    echo'A condicao nao bate';
+}
+
+?>
